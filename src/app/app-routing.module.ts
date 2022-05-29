7d6a9bb5bd003c7d1296cb64e53e58907fe21ff9
Umas Sunavan <fareastsunflower@gmail.com>
2022-05-29 11:54:33 +0800

    Deploy: [fix] dashboard refreshed got 404, update 404.html
    adding index page redirect, remove '/' from wildcard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,11 @@ import { MapViewerComponent } from './modules/map/map-viewer/map-viewer.componen
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'  
+  },
+  {
     path: 'map/:id',
     component: MapViewerComponent
   },
@@ -15,12 +20,12 @@ const routes: Routes = [
   },
   {
     path: '**',
-    redirectTo: '/dashboard'
+    redirectTo: 'dashboard'
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

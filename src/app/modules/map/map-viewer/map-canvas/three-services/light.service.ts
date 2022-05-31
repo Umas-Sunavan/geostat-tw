8ccac9b42a4ea73651654146ebe706446db74fb1
Umas Sunavan <fareastsunflower@gmail.com>
2022-05-31 13:28:18 +0800

    Light: refactor light, disable light helper in production

--- src/app/modules/map/map-viewer/map-canvas/three-services/light.service.ts
+++ src/app/modules/map/map-viewer/map-canvas/three-services/light.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import { AmbientLight, CameraHelper, DirectionalLight, DirectionalLightHelper, HemisphereLight, PointLight, PointLightHelper, Scene } from 'three';
 
 @Injectable({
@@ -14,32 +15,29 @@ export class LightService {
   }
 
   makePointLight = (scene: Scene) => {
-    const light = new PointLight(0xffffff, 0.5)
-    light.position.set(40, 20, 10)
-    const pointLightHelper = new PointLightHelper(light, 5, 0x000000)
-    // scene.add(light)
-    // scene.add(pointLightHelper)
-    const dLight = new DirectionalLight(0xffffff,0.1)
-    const dLightHelper = new DirectionalLightHelper(dLight, 10)
+    const light = new DirectionalLight(0xffffff,0.1)
+    const lightHelper = new DirectionalLightHelper(light, 10)
 
-    const cameraHelper = new CameraHelper(dLight.shadow.camera);
-    scene.add(cameraHelper)
-    dLight.castShadow = true
-    dLight.position.set(35, 40, 20)
-    dLight.shadow.camera.bottom = -20
-    dLight.shadow.camera.top = 10
-    dLight.shadow.camera.left = -20
-    dLight.shadow.camera.right = 20
+    const shadowHelper = new CameraHelper(light.shadow.camera);
+    light.castShadow = true
+    light.position.set(35, 40, 20)
+    light.shadow.camera.bottom = -20
+    light.shadow.camera.top = 10
+    light.shadow.camera.left = -20
+    light.shadow.camera.right = 20
     
-    dLight.target.position.set(20, 10, 30)
-    scene.add(dLight)
-    scene.add(dLightHelper)
+    light.target.position.set(20, 10, 30)
+    scene.add(light)
+    if (!environment.production) {
+      scene.add(lightHelper)
+      scene.add(shadowHelper)
+    }
       
     // update the light target's matrixWorld because it's needed by the helper
-    dLight.target.updateMatrixWorld();
-    dLightHelper.update();
-    dLight.shadow.camera.updateProjectionMatrix();
-    cameraHelper.update();
+    light.target.updateMatrixWorld();
+    lightHelper.update();
+    light.shadow.camera.updateProjectionMatrix();
+    shadowHelper.update();
   }
 
   makeAmbientLight = (scene: Scene) => {

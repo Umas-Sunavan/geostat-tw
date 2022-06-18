587543042adcfe2255140f290ee4547c27fa31a5
Umas Sunavan <fareastsunflower@gmail.com>
2022-06-18 19:10:08 +0800

    Pin Sheet: able to create sheet by api

--- src/app/shared/add-pin-sheet/add-pin-sheet.component.ts
+++ src/app/shared/add-pin-sheet/add-pin-sheet.component.ts
@@ -1,5 +1,7 @@
 /// <reference types="@types/gapi.client.sheets" />
+/// <reference types="@types/gapi.client.drive" />
 /// <reference types="@types/gapi.auth2" />
+/// <reference types="@types/google-one-tap" />
 
 import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Input, NgZone, OnInit, Output } from '@angular/core';
@@ -8,6 +10,8 @@ import { CategoryService } from 'src/app/modules/map/map-viewer/map-canvas/categ
 import { AnimateService } from 'src/app/modules/map/map-viewer/map-canvas/three-services/animate.service';
 import { Object3D } from 'three';
 import {} from 'googleapis'
+import { GoogleAuth } from 'google-auth-library';
+// import * as google from 'google-one-tap';
 
 @Component({
   selector: 'add-pin-sheet-component',
@@ -44,6 +48,7 @@ export class AddPinSheetComponent implements OnInit {
 
   ngOnInit(): void {
     this.checkUrlAccessable()
+    // this.initOAuth2()
     this.gapiLoaded()
     this.gisLoaded()
     // this.isLoaded$.subscribe(value => {
@@ -205,7 +210,7 @@ export class AddPinSheetComponent implements OnInit {
   CLIENT_ID = '349482155640-denm4e3bg0eseughru3krlrlocgjdghj.apps.googleusercontent.com';
   // Authorization scopes required by the API; multiple scopes can be
   // included, separated by spaces.
-  SCOPES = 'https://www.googleapis.com/auth/spreadsheets';
+  SCOPES = 'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive.metadata.readonly';
 
   intializeGapiClient = async () => {
     // Discovery doc URL for APIs used by the quickstart
@@ -233,6 +238,164 @@ export class AddPinSheetComponent implements OnInit {
 
   exampleContent = ''
 
+  batchUpdateCellData = (response:gapi.client.Response<gapi.client.sheets.Spreadsheet>) => {
+    console.log('response.result.spreadsheetUrl', response.result.spreadsheetId);
+    console.log('response.result.spreadsheetUrl', response.result.spreadsheetUrl);
+    var params = {
+      // The spreadsheet to apply the updates to.
+      spreadsheetId: response.result.spreadsheetId!,  // TODO: Update placeholder value.
+    };
+    var batchUpdateSpreadsheetRequestBody:gapi.client.sheets.BatchUpdateSpreadsheetRequest = {
+      // A list of updates to apply to the spreadsheet.
+      // Requests will be applied in the order they are specified.
+      // If any request is not valid, no requests will be applied.
+      requests: [
+        { 
+         pasteData: {
+          data: '落點名稱',
+          type: "PASTE_NORMAL",
+          delimiter: ",",
+          coordinate: {
+            sheetId: 0,
+            rowIndex: 0,
+            columnIndex: 0
+            }
+          }
+        },
+        { 
+         pasteData: {
+          data: '落點地址',
+          type: "PASTE_NORMAL",
+          delimiter: ",",
+          coordinate: {
+            sheetId: 0,
+            rowIndex: 0,
+            columnIndex: 1
+            }
+          }
+        },
+        { 
+         pasteData: {
+          data: '範例落點',
+          type: "PASTE_NORMAL",
+          delimiter: ",",
+          coordinate: {
+            sheetId: 0,
+            rowIndex: 1,
+            columnIndex: 0
+            }
+          }
+        },
+        { 
+         pasteData: {
+          data: '台北市中華路二段56號',
+          type: "PASTE_NORMAL",
+          delimiter: ",",
+          coordinate: {
+            sheetId: 0,
+            rowIndex: 1,
+            columnIndex: 1
+            }
+          }
+        },
+        {
+          "repeatCell": {
+            "range": {
+              "sheetId": +response.result.spreadsheetId!,
+              "startRowIndex": 0,
+              "endRowIndex": 1,
+              "startColumnIndex": 0,
+              "endColumnIndex":2
+            },
+            "cell": {
+              "userEnteredFormat": {
+                "backgroundColor": {
+                  "red": 37/256,
+                  "green": 85/256,
+                  "blue": 122/256
+                },
+                "horizontalAlignment" : "CENTER",
+                "textFormat": {
+                  "foregroundColor": {
+                    "red": 1,
+                    "green": 1,
+                    "blue": 1
+                  },
+                  "fontSize": 12,
+                  "bold": true
+                }
+              }
+            },
+            "fields": "userEnteredFormat(backgroundColor,textFormat,horizontalAlignment)"
+          }
+        },
+        {
+          "repeatCell": {
+            "range": {
+              "sheetId": +response.result.spreadsheetId!,
+              "startRowIndex": 1,
+              "endRowIndex": 99,
+              "startColumnIndex": 0,
+              "endColumnIndex":2
+            },
+            "cell": {
+              "userEnteredFormat": {
+                "backgroundColor": {
+                  "red": 243/256,
+                  "green": 243/256,
+                  "blue": 243/256
+                },
+                "horizontalAlignment" : "LEFT",
+                "textFormat": {
+                  "foregroundColor": {
+                    "red": 0,
+                    "green": 0,
+                    "blue": 0
+                  },
+                  "fontSize": 11,
+                  "bold": false
+                }
+              }
+            },
+            "fields": "userEnteredFormat(backgroundColor,textFormat,horizontalAlignment)"
+          }
+        },
+        {
+          "updateDimensionProperties": {
+            "properties": {
+              "pixelSize": 300,
+            },
+            "fields": "*",
+          
+            // Union field dimension_range can be only one of the following:
+            "range": {
+              "sheetId": +response.result.spreadsheetId!,
+              "dimension": "COLUMNS", 
+              "startIndex": 1,
+              "endIndex":2
+            }
+            // End of list of possible types for union field dimension_range.
+          }
+        },
+        {
+          "updateSheetProperties": {
+            "properties": {
+              "sheetId": +response.result.spreadsheetId!,
+              "gridProperties": {
+                "frozenRowCount": 1,
+              }
+            },
+            "fields": "gridProperties.frozenRowCount"
+          }
+        }
+        
+     ]
+
+      // TODO: Add desired properties to the request body.
+    };
+    return gapi.client.sheets.spreadsheets.batchUpdate(params, batchUpdateSpreadsheetRequestBody)
+  }
+
   listMajors = async () => {
     let response;
     try {
@@ -242,35 +405,52 @@ export class AddPinSheetComponent implements OnInit {
         properties: {
           title: `${this.mapName}的地址資料`
         }
-      }).then((response:gapi.client.Response<gapi.client.sheets.Spreadsheet>) => {
-        console.log('response.result.spreadsheetUrl', response.result.spreadsheetId);
-        console.log('response.result.spreadsheetUrl', response.result.spreadsheetUrl);
-        var params = {
-          // The spreadsheet to apply the updates to.
-          spreadsheetId: response.result.spreadsheetId!,  // TODO: Update placeholder value.
-        };
-        var batchUpdateSpreadsheetRequestBody:gapi.client.sheets.BatchUpdateSpreadsheetRequest = {
-          // A list of updates to apply to the spreadsheet.
-          // Requests will be applied in the order they are specified.
-          // If any request is not valid, no requests will be applied.
-          requests: [
-            { 
-             pasteData: {
-              data: 'myData',
-              type: "PASTE_NORMAL",
-              delimiter: ",",
-              coordinate: {
-                sheetId: 0,
-                rowIndex: 0,
-                }
-              }
-            }
-         ]
-  
-          // TODO: Add desired properties to the request body.
-        };
-        return gapi.client.sheets.spreadsheets.batchUpdate(params, batchUpdateSpreadsheetRequestBody)
       })
+        .then( sheet => this.batchUpdateCellData(sheet))
+        // .then( sheet => {
+        //   console.log(sheet.result.spreadsheetId);
+        //   return gapi.client.drive.files.list({
+        //     q: 'mimeType=\'application/vnd.google-apps.spreadsheet\'',
+        //     pageSize: 100,
+        //     fields: 'files(id, name)',
+        //   })
+        // })
+        .then( result => {
+          console.log(result.result.spreadsheetId);
+          setInterval( ()=> {
+            const params = {
+              // The ID of the spreadsheet to retrieve data from.
+              spreadsheetId: result.result.spreadsheetId!,  // TODO: Update placeholder value.
+      
+              // The A1 notation of the values to retrieve.
+              range: 'Sheet1!A:B',  // TODO: Update placeholder value.
+      
+              // How values should be represented in the output.
+              // The default render option is ValueRenderOption.FORMATTED_VALUE.
+              valueRenderOption: 'FORMATTED_VALUE',  // TODO: Update placeholder value.
+      
+              // How dates, times, and durations should be represented in the output.
+              // This is ignored if value_render_option is
+              // FORMATTED_VALUE.
+              // The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
+              dateTimeRenderOption: 'FORMATTED_STRING',  // TODO: Update placeholder value.
+            };
+            const request = gapi.client.sheets.spreadsheets.values.get(params);
+            request.then( res => {
+              console.log(res);
+              
+            })
+
+          } , 5000)
+        })
+
+      // gapi.client.drive.files.list({
+      //   'pageSize': 10,
+      //   'fields': 'files(id, name)',
+      // }).then( result =>{
+      //   console.log(result);
+        
+      // })
       response = await gapi.client.sheets.spreadsheets.values.get({
         spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
         range: 'Class Data!A2:E',

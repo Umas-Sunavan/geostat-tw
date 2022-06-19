22c7a1cb1e336641660da7a235843b4fbae44cf3
Umas Sunavan <fareastsunflower@gmail.com>
2022-06-19 21:42:33 +0800

    Pin Sheet: able to make sheet public

--- src/app/shared/add-pin-sheet/add-pin-sheet.component.ts
+++ src/app/shared/add-pin-sheet/add-pin-sheet.component.ts
@@ -25,8 +25,6 @@ export class AddPinSheetComponent implements OnInit {
   constructor(
     private animateService: AnimateService,
     private categoryService: CategoryService,
-    private zone: NgZone,
-    private http: HttpClient
   ) { }
 
 
@@ -34,16 +32,60 @@ export class AddPinSheetComponent implements OnInit {
   public isLoggedIn: boolean = false;
   public user: any;
 
-  signInComponent(): void {
-    this.signIn();
-  };
-
-  signOutComponent(): void {
-    this.signOut();
+  updateSigninStatus = (signedIn: boolean = true) => {
+    // console.log(GoogleAuth);
+    
+    // // @ts-ignore
+    // var user = GoogleAuth.currentUser.get();
+    // var isAuthorized = user.hasGrantedScopes(this.SCOPES);
+    // if (isAuthorized) {
+    //   console.log('You are currently signed in and have granted ' +
+    //   'access to this app.');
+    // } else {
+    //   console.log('You have not authorized this app or you are ' +
+    //   'signed out.');
+    // }
   }
 
-  ngOnInitComponent() {
+  intializeGapiClient = async () => {
+    await gapi.client.init({
+      apiKey: this.API_KEY,
+      discoveryDocs: this.DISCOVERY_DOC,
+      // clientId: this.CLIENT_ID,
+      // scope: this.SCOPES,
+    })
+    // const GoogleAuth = gapi.auth2.getAuthInstance();
 
+    //   // Listen for sign-in state changes.
+    //   console.log(GoogleAuth);
+      
+    //   GoogleAuth.isSignedIn.listen(this.updateSigninStatus);
+
+    //   // Handle initial sign-in state. (Determine if user is already signed in.)
+    //   // const user = GoogleAuth.currentUser.get();
+    //   this.updateSigninStatus();
+
+    //   // // Call handleAuthClick function when user clicks on
+    //   // //      "Sign In/Authorize" button.
+    //   // $('#revoke-access-button').click(function() {
+    //   //   revokeAccess();
+    //   // });
+
+    this.gapiInited = true;
+  }
+
+  gapiLoaded = () => {
+    gapi.load('client', this.intializeGapiClient);
+  }
+ 
+  gisLoaded = () => {
+    // @ts-ignore
+    this.tokenClient = google.accounts.oauth2.initTokenClient({
+      client_id: this.CLIENT_ID,
+      scope: this.SCOPES,
+      callback: '', // defined later
+    });
+    this.gisInited = true;
   }
 
   ngOnInit(): void {
@@ -51,19 +93,6 @@ export class AddPinSheetComponent implements OnInit {
     // this.initOAuth2()
     this.gapiLoaded()
     this.gisLoaded()
-    // this.isLoaded$.subscribe(value => {
-    //   this.authIsLoaded = value;
-    // });
-
-    // this.isLoggedIn$.subscribe(value => {
-    //   this.isLoggedIn = value;
-    // });
-
-    // this.user$.subscribe(value => {
-    //   this.user = value;
-    // });
-
-    // this.loadAuth2();
   }
 
   showSubmitTip: boolean = false
@@ -146,303 +175,247 @@ export class AddPinSheetComponent implements OnInit {
 
   }
 
+  // sheet API
 
-
-
-
-  public auth2: any;
-  public user$: BehaviorSubject<any> = new BehaviorSubject<any>(null);
-  public isLoggedIn$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  public isLoaded$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-
-
-  validateToken(token: string): Observable<any> {
-    return this.http.get<any>(`http://yourServer:3000/validationApi/${token}`);
-  }
-
-  signIn(): void {
-    this.auth2.signIn().then((user: any) => {
-      this.validateToken(user.getAuthResponse().id_token).subscribe(user => {
-        this.zone.run(() => {
-          this.user$.next(user);
-          this.isLoggedIn$.next(true);
-        });
-      },
-        (err) => {
-          console.error(err);
-        });
-    });
-  };
-
-  signOut(): void {
-    this.auth2.signOut().then(() => {
-      this.zone.run(() => {
-        this.isLoggedIn$.next(false);
-        this.user$.next(null);
-      });
-    },
-      (err: any) => {
-        console.error(err);
-      });
-  }
-
-  loadAuth2(): void {
-    gapi.load('auth2', () => {
-      gapi.auth2.init({
-        client_id: '349482155640-denm4e3bg0eseughru3krlrlocgjdghj.apps.googleusercontent.com',
-        scope: 'https://www.googleapis.com/auth/spreadsheets',
-        fetch_basic_profile: true
-      }).then((auth: any) => {
-        this.zone.run(() => {
-          this.auth2 = auth;
-          this.isLoaded$.next(true);
-        });
-      },
-      );
-    });
-  }
-
+  signoutShow = false
+  authorizeShow = false
   gapiInited = false;
   gisInited = false;
   tokenClient?: any;
-  DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
-  API_KEY = 'AIzaSyDkNXYD5Hmohj4E9sJ6yhzI7zTVipIowDE';
-  CLIENT_ID = '349482155640-denm4e3bg0eseughru3krlrlocgjdghj.apps.googleusercontent.com';
+  DISCOVERY_DOC = ['https://sheets.googleapis.com/$discovery/rest?version=v4', 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'];
+  API_KEY = '';
+  CLIENT_ID = '';
   // Authorization scopes required by the API; multiple scopes can be
   // included, separated by spaces.
-  SCOPES = 'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive.metadata.readonly';
+  SCOPES = 'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive.file';
+  sheetId: string = ''
 
-  intializeGapiClient = async () => {
-    // Discovery doc URL for APIs used by the quickstart
-    await gapi.client.init({
-      apiKey: this.API_KEY,
-      discoveryDocs: [this.DISCOVERY_DOC],
-    });
-    this.gapiInited = true;
-  }
+  /**
+   *  Sign in the user upon button click.
+   */
+  handleAuthClick = () => {
+    this.tokenClient.callback = async (resp:any) => {
+      if (resp.error !== undefined) {
+        throw (resp);
+      }
+      this.signoutShow = true
+      this.authorizeShow = true
+      await this.listMajors();
+    };
 
-
-  gapiLoaded = () => {
-    gapi.load('client', this.intializeGapiClient);
-  }
- 
-  gisLoaded = () => {
-    // @ts-ignore
-    this.tokenClient = google.accounts.oauth2.initTokenClient({
-      client_id: this.CLIENT_ID,
-      scope: this.SCOPES,
-      callback: '', // defined later
-    });
-    this.gisInited = true;
+    if (gapi.client.getToken() === null) {      
+      this.tokenClient.requestAccessToken({ prompt: 'consent' });
+    } else {
+      this.tokenClient.requestAccessToken({ prompt: '' });
+    }
   }
 
   exampleContent = ''
 
-  batchUpdateCellData = (response:gapi.client.Response<gapi.client.sheets.Spreadsheet>) => {
-    console.log('response.result.spreadsheetUrl', response.result.spreadsheetId);
-    console.log('response.result.spreadsheetUrl', response.result.spreadsheetUrl);
-    var params = {
-      // The spreadsheet to apply the updates to.
-      spreadsheetId: response.result.spreadsheetId!,  // TODO: Update placeholder value.
-    };
-    var batchUpdateSpreadsheetRequestBody:gapi.client.sheets.BatchUpdateSpreadsheetRequest = {
-      // A list of updates to apply to the spreadsheet.
-      // Requests will be applied in the order they are specified.
-      // If any request is not valid, no requests will be applied.
-      requests: [
-        { 
-         pasteData: {
-          data: '落點名稱',
-          type: "PASTE_NORMAL",
-          delimiter: ",",
-          coordinate: {
-            sheetId: 0,
-            rowIndex: 0,
-            columnIndex: 0
-            }
-          }
-        },
-        { 
-         pasteData: {
-          data: '落點地址',
-          type: "PASTE_NORMAL",
-          delimiter: ",",
-          coordinate: {
-            sheetId: 0,
-            rowIndex: 0,
-            columnIndex: 1
-            }
-          }
-        },
-        { 
-         pasteData: {
-          data: '範例落點',
-          type: "PASTE_NORMAL",
-          delimiter: ",",
-          coordinate: {
-            sheetId: 0,
-            rowIndex: 1,
-            columnIndex: 0
-            }
-          }
-        },
-        { 
-         pasteData: {
-          data: '台北市中華路二段56號',
-          type: "PASTE_NORMAL",
-          delimiter: ",",
-          coordinate: {
-            sheetId: 0,
-            rowIndex: 1,
-            columnIndex: 1
-            }
-          }
-        },
-        {
-          "repeatCell": {
-            "range": {
-              "sheetId": +response.result.spreadsheetId!,
-              "startRowIndex": 0,
-              "endRowIndex": 1,
-              "startColumnIndex": 0,
-              "endColumnIndex":2
-            },
-            "cell": {
-              "userEnteredFormat": {
-                "backgroundColor": {
-                  "red": 37/256,
-                  "green": 85/256,
-                  "blue": 122/256
-                },
-                "horizontalAlignment" : "CENTER",
-                "textFormat": {
-                  "foregroundColor": {
-                    "red": 1,
-                    "green": 1,
-                    "blue": 1
-                  },
-                  "fontSize": 12,
-                  "bold": true
-                }
-              }
-            },
-            "fields": "userEnteredFormat(backgroundColor,textFormat,horizontalAlignment)"
-          }
-        },
-        {
-          "repeatCell": {
-            "range": {
-              "sheetId": +response.result.spreadsheetId!,
-              "startRowIndex": 1,
-              "endRowIndex": 99,
-              "startColumnIndex": 0,
-              "endColumnIndex":2
-            },
-            "cell": {
-              "userEnteredFormat": {
-                "backgroundColor": {
-                  "red": 243/256,
-                  "green": 243/256,
-                  "blue": 243/256
-                },
-                "horizontalAlignment" : "LEFT",
-                "textFormat": {
-                  "foregroundColor": {
-                    "red": 0,
-                    "green": 0,
-                    "blue": 0
-                  },
-                  "fontSize": 11,
-                  "bold": false
-                }
-              }
-            },
-            "fields": "userEnteredFormat(backgroundColor,textFormat,horizontalAlignment)"
-          }
-        },
-        {
-          "updateDimensionProperties": {
-            "properties": {
-              "pixelSize": 300,
-            },
-            "fields": "*",
-          
-            // Union field dimension_range can be only one of the following:
-            "range": {
-              "sheetId": +response.result.spreadsheetId!,
-              "dimension": "COLUMNS", 
-              "startIndex": 1,
-              "endIndex":2
-            }
-            // End of list of possible types for union field dimension_range.
-          }
-        },
-        {
-          "updateSheetProperties": {
-            "properties": {
-              "sheetId": +response.result.spreadsheetId!,
-              "gridProperties": {
-                "frozenRowCount": 1,
-              }
-            },
-            "fields": "gridProperties.frozenRowCount"
-          }
+  pastSheetInitData: gapi.client.sheets.Request[] = [
+    { 
+     pasteData: {
+      data: '落點名稱',
+      type: "PASTE_NORMAL",
+      delimiter: ",",
+      coordinate: {
+        sheetId: 0,
+        rowIndex: 0,
+        columnIndex: 0
         }
-        
-     ]
+      }
+    },
+    { 
+     pasteData: {
+      data: '落點地址',
+      type: "PASTE_NORMAL",
+      delimiter: ",",
+      coordinate: {
+        sheetId: 0,
+        rowIndex: 0,
+        columnIndex: 1
+        }
+      }
+    },
+    { 
+     pasteData: {
+      data: '範例落點',
+      type: "PASTE_NORMAL",
+      delimiter: ",",
+      coordinate: {
+        sheetId: 0,
+        rowIndex: 1,
+        columnIndex: 0
+        }
+      }
+    },
+    { 
+     pasteData: {
+      data: '台北市中華路二段56號',
+      type: "PASTE_NORMAL",
+      delimiter: ",",
+      coordinate: {
+        sheetId: 0,
+        rowIndex: 1,
+        columnIndex: 1
+        }
+      }
+  }]
 
-      // TODO: Add desired properties to the request body.
+  formatSheetHeaderRow: gapi.client.sheets.Request[] = [
+    {
+      "repeatCell": {
+        "range": {
+          "sheetId": +this.sheetId,
+          "startRowIndex": 0,
+          "endRowIndex": 1,
+          "startColumnIndex": 0,
+          "endColumnIndex":2
+        },
+        "cell": {
+          "userEnteredFormat": {
+            "backgroundColor": {
+              "red": 37/256,
+              "green": 85/256,
+              "blue": 122/256
+            },
+            "horizontalAlignment" : "CENTER",
+            "textFormat": {
+              "foregroundColor": {
+                "red": 1,
+                "green": 1,
+                "blue": 1
+              },
+              "fontSize": 12,
+              "bold": true
+            }
+          }
+        },
+        "fields": "userEnteredFormat(backgroundColor,textFormat,horizontalAlignment)"
+      }
+    },
+  ]
+
+  formatSheetBodyRows: gapi.client.sheets.Request[] = [ 
+    {
+      "repeatCell": {
+        "range": {
+          "sheetId": +this.sheetId,
+          "startRowIndex": 1,
+          "endRowIndex": 99,
+          "startColumnIndex": 0,
+          "endColumnIndex":2
+        },
+        "cell": {
+          "userEnteredFormat": {
+            "backgroundColor": {
+              "red": 243/256,
+              "green": 243/256,
+              "blue": 243/256
+            },
+            "horizontalAlignment" : "LEFT",
+            "textFormat": {
+              "foregroundColor": {
+                "red": 0,
+                "green": 0,
+                "blue": 0
+              },
+              "fontSize": 11,
+              "bold": false
+            }
+          }
+        },
+        "fields": "userEnteredFormat(backgroundColor,textFormat,horizontalAlignment)"
+      }
+    },
+  ]
+
+  changeCellWidth: gapi.client.sheets.Request[] = [ 
+    {
+      "updateDimensionProperties": {
+        "properties": {
+          "pixelSize": 300,
+        },
+        "fields": "*",
+      
+        // Union field dimension_range can be only one of the following:
+        "range": {
+          "sheetId": +this.sheetId,
+          "dimension": "COLUMNS", 
+          "startIndex": 1,
+          "endIndex":2
+        }
+        // End of list of possible types for union field dimension_range.
+      }
+    },
+  ]
+
+  freezeHeaderRow: gapi.client.sheets.Request[] = [ 
+    {
+      "updateSheetProperties": {
+        "properties": {
+          "sheetId": +this.sheetId,
+          "gridProperties": {
+            "frozenRowCount": 1,
+          }
+        },
+        "fields": "gridProperties.frozenRowCount"
+      }
+    }
+  ]
+
+  batchUpdateCellData = (response:gapi.client.Response<gapi.client.sheets.Spreadsheet>) => {
+    var params = {spreadsheetId: response.result.spreadsheetId!}
+    var batchUpdateSpreadsheetRequestBody:gapi.client.sheets.BatchUpdateSpreadsheetRequest = {
+      requests: [
+        ...this.pastSheetInitData,
+        ...this.formatSheetHeaderRow,
+        ...this.formatSheetBodyRows,
+        ...this.changeCellWidth,
+        ...this.freezeHeaderRow
+     ]
     };
     return gapi.client.sheets.spreadsheets.batchUpdate(params, batchUpdateSpreadsheetRequestBody)
+  }
+
+  createPermission = (response:gapi.client.Response<gapi.client.sheets.Spreadsheet>) => {
+    const fileId = response.result.spreadsheetId
+    if (!fileId) throw new Error("no sheet file id found when creating file permission");
+    const body = {
+      "role": "writer",
+      "type": "anyone"
+    }
+    return gapi.client.drive.permissions.create({ fileId }, body)
   }
 
   listMajors = async () => {
     let response;
     try {
-      // Fetch first 10 files
       gapi.client.sheets.spreadsheets.create({
         // @ts-ignore
         properties: {
           title: `${this.mapName}的地址資料`
         }
       })
-        .then( sheet => this.batchUpdateCellData(sheet))
-        // .then( sheet => {
-        //   console.log(sheet.result.spreadsheetId);
-        //   return gapi.client.drive.files.list({
-        //     q: 'mimeType=\'application/vnd.google-apps.spreadsheet\'',
-        //     pageSize: 100,
-        //     fields: 'files(id, name)',
-        //   })
-        // })
+        .then( sheet =>  this.batchUpdateCellData(sheet))
         .then( result => {
-          console.log(result.result.spreadsheetId);
-          setInterval( ()=> {
-            const params = {
-              // The ID of the spreadsheet to retrieve data from.
-              spreadsheetId: result.result.spreadsheetId!,  // TODO: Update placeholder value.
-      
-              // The A1 notation of the values to retrieve.
-              range: 'Sheet1!A:B',  // TODO: Update placeholder value.
-      
-              // How values should be represented in the output.
-              // The default render option is ValueRenderOption.FORMATTED_VALUE.
-              valueRenderOption: 'FORMATTED_VALUE',  // TODO: Update placeholder value.
-      
-              // How dates, times, and durations should be represented in the output.
-              // This is ignored if value_render_option is
-              // FORMATTED_VALUE.
-              // The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
-              dateTimeRenderOption: 'FORMATTED_STRING',  // TODO: Update placeholder value.
-            };
-            const request = gapi.client.sheets.spreadsheets.values.get(params);
-            request.then( res => {
-              console.log(res);
-              
-            })
-
-          } , 5000)
+          return this.createPermission(result)
+          // setInterval( ()=> {
+          //   const params = {
+          //     spreadsheetId: result.result.spreadsheetId!,
+          //     range: 'Sheet1!A:B',
+          //     valueRenderOption: 'FORMATTED_VALUE',
+          //     dateTimeRenderOption: 'FORMATTED_STRING',
+          //   };
+          //   const request = gapi.client.sheets.spreadsheets.values.get(params);
+          // } , 5000)
         })
+        .then( response => {
+          console.log(response.result.permissionDetails);
+          
+        }
+          
+        )
 
       // gapi.client.drive.files.list({
       //   'pageSize': 10,
@@ -469,31 +442,5 @@ export class AddPinSheetComponent implements OnInit {
         (str:any, row:any) => `${str}${row[0]}, ${row[4]}\n`,
         'Name, Major:\n');
     this.exampleContent = output;
-  }
-
-  signoutShow = false
-  authorizeShow = false
-
-  /**
-   *  Sign in the user upon button click.
-   */
-  handleAuthClick = () => {
-    this.tokenClient.callback = async (resp:any) => {
-      if (resp.error !== undefined) {
-        throw (resp);
-      }
-      this.signoutShow = true
-      this.authorizeShow = true
-      await this.listMajors();
-    };
-
-    if (gapi.client.getToken() === null) {
-      // Prompt the user to select a Google Account and ask for consent to share their data
-      // when establishing a new session.
-      this.tokenClient.requestAccessToken({ prompt: 'consent' });
-    } else {
-      // Skip display of account chooser and consent dialog for an existing session.
-      this.tokenClient.requestAccessToken({ prompt: '' });
-    }
   }
 }
